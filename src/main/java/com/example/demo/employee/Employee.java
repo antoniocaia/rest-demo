6284b591ab3b6d7b83f1d70ab3b749fa7f348b20
Antonio Caia <antonio.caia96@gmail.com>
2021-03-31 15:38:59 +0200

    Add Lombok
    Used on Employee

--- src/main/java/com/example/demo/employee/Employee.java
+++ src/main/java/com/example/demo/employee/Employee.java
@@ -1,82 +1,40 @@
 package com.example.demo.employee;
 
-import java.util.Objects;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-// Javax annotation
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Entity
-public class Employee {
-	// Javax annotation
+@Data @NoArgsConstructor public class Employee {
+	
 	private @Id @GeneratedValue Long id;
 	private String name;
 	private String role;
 	// @OneToMany should always used with 'mappedBy' to avoid inconsistency. 
 	// Furthermore, the "many" entity ('Order' class) should have a @ManyToOne annotation without 'mappedBy'
 	// https://www.baeldung.com/hibernate-one-to-many
+	// @OneToMany(mappedBy = "employee")	
+	// private Set<Order> orders;
 	
-	public Employee() {}
-	
-	// Only used to preload
+	// Only used for loading resources
 	public Employee(String name, String role) {
 		this.name = name;
 		this.role = role;
 	}
-	// Only used to preload
-	public Employee(long id, String name, String role) {
+	
+	// Only used for loading resources
+	public Employee(Long id) {	
+		this.id = id;
+	}
+
+	// Only used for loading resources
+	public Employee(Long id, String name, String role) {
 		this.id = id;
 		this.name = name;
 		this.role = role;
 	}
 	
-	public Employee(Long id) {
-		this.id = id;
-	}
-
-	public Long getId() {
-		return this.id;
-	}
-
-	public String getName() {
-		return this.name;
-	}
-
-	public String getRole() {
-		return this.role;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public void setRole(String role) {
-		this.role = role;
-	}
-
-	@Override
-	public boolean equals(Object o) {
-		if (this == o)
-			return true;
-		if (!(o instanceof Employee))
-			return false;
-		Employee employee = (Employee) o;
-		return Objects.equals(this.id, employee.id) && Objects.equals(this.name, employee.name)
-				&& Objects.equals(this.role, employee.role);
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(this.id, this.name, this.role);
-	}
-
-	@Override
-	public String toString() {
-		return "Employee{" + "id=" + this.id + ", name='" + this.name + '\'' + ", role='" + this.role + '\'' + '}';
-	}
 }
