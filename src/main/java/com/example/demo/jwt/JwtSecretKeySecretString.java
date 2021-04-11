c5c0315755ed9b60f9774932b52f98ad0f3e5358
Antonio Caia <antonio.caia96@gmail.com>
2021-04-11 21:21:26 +0200

    EDU: little fix

--- src/main/java/com/example/demo/jwt/JwtSecretKeySecretString.java
+++ src/main/java/com/example/demo/jwt/JwtSecretKeySecretString.java
@@ -3,7 +3,6 @@ package com.example.demo.jwt;
 import javax.crypto.SecretKey;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.jsonwebtoken.io.Decoders;
@@ -15,7 +14,7 @@ public class JwtSecretKeySecretString {
 	@Autowired
 	private JwtConfig jwtConfig;
 
-	@Bean
+	// @Bean Commenting this line so there is no collision with the actual bean
 	public SecretKey getSecretKey() {
 		return Keys.hmacShaKeyFor(Decoders.BASE64URL.decode(jwtConfig.getSecretString()));
 	}
